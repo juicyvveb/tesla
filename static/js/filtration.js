1e5cceeace199591c53a4e3a8907c8ebf0d62079
luisfendi@protonmail.com <luisfendi@protonmail.com>
2023-02-03 10:07:55 +0300

    first realiziton

--- static/js/filtration.js
+++ static/js/filtration.js
@@ -1,5 +1,5 @@
 class Tesla {
-  constructor(price, range, speed, mph, colors, wheels, interior) {
+  constructor(price, range, speed, mph, colors, wheels, interior, imgType) {
     this.price = price;
     this.range = range;
     this.speed = speed;
@@ -7,6 +7,7 @@ class Tesla {
     this.colors = colors;
     this.wheels = wheels;
     this.interior = interior;
+    this.imgType = imgType
   }
   static getPrice() {
     return this.price;
@@ -23,9 +24,10 @@ class ModelS extends Tesla {
     wheels,
     interior,
     sterring,
-    title
+    title,
+    imgType //type for images folder(type1/type2)
   ) {
-    super(price, range, speed, mph, colors, wheels, interior);
+    super(price, range, speed, mph, colors, wheels, interior, imgType);
     this.sterring = sterring;
     this.title = title;
   }
@@ -40,9 +42,10 @@ class ModelSPlaid extends ModelS {
     wheels,
     interior,
     sterring,
-    title
+    title,
+    imgType //type for images folder(type1/type2)
   ) {
-    super(price, range, speed, mph, colors, wheels, interior, sterring, title);
+    super(price, range, speed, mph, colors, wheels, interior, sterring, title, imgType);
   }
 }
 
@@ -50,8 +53,7 @@ const model_s = new ModelS(
   94900,
   396,
   200,
-  3.1,
-  {
+  3.1, {
     silver: {
       description: 'Полуночный серебристый металлик',
       alt: 'Серый цвет',
@@ -64,31 +66,53 @@ const model_s = new ModelS(
       description: 'Красное многослойное покрытие',
       alt: 'Красный цвет',
     },
-  },
-  {
+  }, {
     tempest: {
       size: 19,
       title: 'Tempest',
+      imgFolder: 'type1'
     },
     arachnid: {
       size: 21,
       title: 'Arachnid',
+      imgFolder: 'type2'
     },
+  }, {
+    black: {
+      id: 'interiorBlack',
+      description: 'Черный салон',
+    },
+    white: {
+      id: 'interiorWhite',
+      description: 'Белый салон',
+    },
+    cream: {
+      id: 'interiorCream',
+      description: 'Кремовый салон',
+    }
+  }, {
+    standart: {
+      type: 'wheel-b',
+      description: "Стандартный руль",
+      id: "controlStandart",
+      imgFolder: 'type1'
+    },
+    yoke: {
+      type: 'wheel-y',
+      description: "Прямоугольный руль",
+      id: "controlYoke",
+      imgFolder: 'type2'
+    }
   },
-  'black,white,cream',
-  [
-    { type: 'wheel-b', description: 'стандартный руль', id: 'controlStandart' },
-    { type: 'wheel-y', description: 'прямоугольный руль', id: 'controlYoke' },
-  ],
-  'Model S'
+  'Model S',
+  'type1'
 );
 
 const model_s_plaid = new ModelSPlaid(
   114900,
   416,
   250,
-  1.9,
-  {
+  1.9, {
     white: {
       description: 'Жемчужно-белое многослойное покрытие',
       alt: 'Белый цвет',
@@ -109,72 +133,66 @@ const model_s_plaid = new ModelSPlaid(
       description: 'Красное многослойное покрытие',
       alt: 'Красный цвет',
     },
-  },
-  {
+  }, {
     tempest: {
       size: 19,
       title: 'Tempest',
+      imgFolder: 'type1'
     },
     arachnid: {
       size: 21,
       title: 'Arachnid',
+      imgFolder: 'type2'
     },
+  }, {
+    black: {
+      id: 'interiorBlack',
+      description: 'Черный салон',
+    },
+    white: {
+      id: 'interiorWhite',
+      description: 'Белый салон',
+    },
+    cream: {
+      id: 'interiorCream',
+      description: 'Кремовый салон',
+    }
+  }, {
+    standart: {
+      type: 'wheel-b',
+      description: "Стандартный руль",
+      id: "controlStandart",
+      imgFolder: 'type1'
+    },
+    yoke: {
+      type: 'wheel-y',
+      description: "Прямоугольный руль",
+      id: "controlYoke",
+      imgFolder: 'type2'
+    }
   },
-  'black,white,cream',
-  [
-    { type: 'wheel-b', description: 'стандартный руль', id: 'controlStandart' },
-    { type: 'wheel-y', description: 'прямоугольный руль', id: 'controlYoke' },
-  ],
-  'Model S Plaid'
+  'Model S Plaid',
+  'type2'
 );
 
 //варианты моделей для страницы ModelS
-const variants = { 'model-s': model_s, 'model-s-plaid': model_s_plaid };
+const variants = {
+  'model-s': model_s,
+  'model-s-plaid': model_s_plaid
+};
+
 let currentVariant = model_s; //по дефолту грузим modelS
 let C = currentVariant;
 const inputsWithCurrent = document.querySelectorAll('.prices__input');
-
 // console.log(inputWithCurrent.value)
 [...inputsWithCurrent].forEach((input) => {
   input.addEventListener('input', (e) => {
     currentVariant = variants[e.target.value];
     pasteData();
+    fillAllSlides()
   });
 });
 
-// const colorFilter = {
-//   white: {
-//     description: 'Жемчужно-белое многослойное покрытие',
-//     alt: 'Белый цвет',
-//   },
-//   black: {
-//     description: 'Сплошной черный',
-//     alt: 'Черный цвет',
-//   },
-//   silver: {
-//     description: 'Полуночный серебристый металлик',
-//     alt: 'Серый цвет',
-//   },
-//   blue: {
-//     description: 'Глубокий синий металлик',
-//     alt: 'Синий цвет',
-//   },
-//   red: {
-//     description: 'Красное многослойное покрытие',
-//     alt: 'Красный цвет',
-//   },
-// };
-
-const wheelsFilter = {
-  tempest: {
-    size: 19,
-    title: 'Tempest',
-  },
-  arachnid: {
-    size: 21,
-    title: 'Arachnid',
-  },
-};
 
 //gettingdynamic inform slots
 const title = document.querySelector('.detailsCar__title'),
@@ -195,6 +213,7 @@ const title = document.querySelector('.detailsCar__title'),
       currentVariant = variants[input.value];
     }
     pasteData();
+    fillAllSlides()
   });
 })();
 
@@ -217,11 +236,10 @@ function buildColorFilterBlock() {
   //   const availableColors = Object.keys(colorFilter).filter(color => currentVariant.colors.split(',').includes(color))
   const availableColors = Object.keys(currentVariant.colors);
   //   console.log(availableColors.white)
-  availableColors.forEach((color) => {
-    console.log(color);
+  availableColors.forEach((color, i) => {
     const colorItem = document.createElement('div');
     colorItem.classList.add('filterCar__item');
-    colorItem.innerHTML = `<input class="filterCar__input" type="radio" data-descr="Жемчужно-белое многослойное покрытие" data-state="${color}" name="paint" id="paint${color}">
+    colorItem.innerHTML = `<input class="filterCar__input color" value="${color}" checked=${i === 0} type="radio" data-descr="Жемчужно-белое многослойное покрытие" data-state="${color}" name="paint" id="paint${color}">
     <label class="filterCar__label" for="paint${color}"><img src="static/images/img/filter/paint/${color}.png" alt="${currentVariant.colors[color].alt}"></label>`;
     colors.appendChild(colorItem);
   });
@@ -230,11 +248,11 @@ function buildColorFilterBlock() {
 function buildWheelsFilterBlock() {
   wheels.innerHTML = '';
   const availableWheels = Object.keys(currentVariant.wheels);
-  availableWheels.forEach((type) => {
+  availableWheels.forEach((type, i) => {
     const wheelItem = document.createElement('div');
     wheelItem.classList.add('filterCar__item');
     wheelItem.innerHTML = `
-        <input class="filterCar__input" type="radio" data-descr="${C.wheels[type].size}-дюймовые колеса ${C.wheels[type].title}" name="wheels" data-state="tempest" id="wheels${C.wheels[type].title}">
+        <input class="filterCar__input wheels" value="${C.wheels[type].imgFolder}" checked=${i === 0} type="radio" data-descr="${C.wheels[type].size}-дюймовые колеса ${C.wheels[type].title}" name="wheels" data-state="tempest" id="wheels${C.wheels[type].title}">
         <label class="filterCar__label" for="wheels${C.wheels[type].title}"><img src="static/images/img/filter/wheels/${type}.png" alt="${C.wheels[type].size}-дюймовые колеса ${title}"></label>
         `;
     wheels.appendChild(wheelItem);
@@ -243,12 +261,13 @@ function buildWheelsFilterBlock() {
 
 function buildInterierBlock() {
   interier.innerHTML = '';
-  currentVariant.interior.split(',').forEach((style) => {
+  const availableInteriers = Object.keys(C.interior);
+  availableInteriers.forEach((type, i) => {
     const interierItem = document.createElement('div');
     interierItem.classList.add('filterCar__item');
     interierItem.innerHTML = `
-    <input class="filterCar__input" type="radio" data-descr="Кремовый салон" name="interior" data-state="${style}" id="interior${style}">
-    <label class="filterCar__label" for="interior${style}"><img src="static/images/img/filter/interior/${style}.png" alt="${style}"></label>
+    <input class="filterCar__input interier" value="${type}" checked=${i === 0} type="radio" data-descr="${C.interior[type].description}" name="interior" data-state="${type}" id="${C.interior[type].id}">
+    <label class="filterCar__label" for="${C.interior[type].id}"><img src="static/images/img/filter/interior/${type}.png" alt="${C.interior[type].description}}"></label>
     `;
     interier.appendChild(interierItem);
   });
@@ -256,15 +275,67 @@ function buildInterierBlock() {
 
 function buildSteeringBlock() {
   steering.innerHTML = '';
-  currentVariant.sterring.forEach(({ type, description, id }) => {
+  const availableSterring = Object.keys(C.sterring);
+  availableSterring.forEach((type, i) => {
     const sterringType = document.createElement('div');
     sterringType.classList.add('filterCar__item');
     sterringType.innerHTML = `
-    <input class="filterCar__input" type="radio" data-descr="${description}" name="control" data-state="${type}" id="${id}">
-    <label class="filterCar__label" for="${id}"><img src="static/images/img/filter/control/${type}.png" alt="Черный салон"></label>
+    <input class="filterCar__input steering" value="${C.sterring[type].imgFolder}" checked=${i === 0} type="radio" data-descr="${C.sterring[type].description}" name="control" data-state="${type}" id="${C.sterring[type].id}">
+    <label class="filterCar__label" for="${C.sterring[type].id}"><img src="static/images/img/filter/control/${C.sterring[type].type}.png" alt="${C.sterring[type].description}"></label>
     `;
     steering.appendChild(sterringType);
   });
 }
 
-//установка дефолтных фильтров
+
+
+function getFilterCategorieValue(selector) {
+  const nodes = document.querySelectorAll(selector);
+  let choosedValue = [...nodes].filter(input => input.checked)[0].value;
+  [...nodes].forEach(input => {
+    input.addEventListener('input', (e) => {
+      choosedValue = input.value;
+    })
+  })
+  return choosedValue
+}
+
+function fillAllSlides() {
+  let choosedSteering = getFilterCategorieValue('.filterCar__input.steering')
+  let choosedInterier = getFilterCategorieValue('.filterCar__input.interier')
+  let choosedWheel = getFilterCategorieValue('.filterCar__input.wheels')
+  let choosedColor = getFilterCategorieValue('.filterCar__input.color')
+
+  function fillColorImage() {
+    const block = document.querySelector('.filterCar__look.paint');
+    block.querySelector('img').src = `
+      static/images/img/models/wheels/${choosedWheel}/interior/${choosedInterier}/${choosedColor}/2.jpg
+      `
+  }
+
+  function fillWheelsImage() {
+    const block = document.querySelector('.filterCar__look.wheels');
+    block.querySelector('img').src = `
+      static/images/img/models/wheels/${choosedWheel}/interior/${choosedInterier}/${choosedColor}/2.jpg
+      `
+  }
+
+  function fillInterierImage() {
+    const block = document.querySelector('.filterCar__look.interior');
+    block.querySelector('img').src = `
+      static/images/img/models/interior/${choosedInterier}/${choosedColor}/5.jpg
+      `
+  }
+
+  function fillSteeringImage() {
+    const block = document.querySelector('.filterCar__look.control');
+    block.querySelector('img').src = `
+      static/images/img/models/handlebar/${choosedSteering}/interior/${choosedInterier}/${choosedColor}/1.jpg
+      `
+  }
+
+  fillColorImage()
+  fillWheelsImage()
+  fillInterierImage()
+  fillSteeringImage()
+}
